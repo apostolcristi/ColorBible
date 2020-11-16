b6700236f9e0d21b1590673b62ed9990ab9cdd62
Apostol Cristi <apostol.cristi@yahoo.com>
2020-11-16 21:19:17 +0200

    Added new fonts + implemented pseudo IAP
    Styled the buttons, founded the font for the GUI buttons and also implemented the pseudo in app purchases (just need the google play console and the app store accounts)

---
+++ Assets/Script/PurchaseButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseButton : MonoBehaviour
+{
+    public enum PurchaseType
+    {
+        removeLock
+    };
+
+    public PurchaseType purchaseType;
+
+    public void ClickPurchaseButton()
+    {
+        switch (purchaseType)
+        {case PurchaseType.removeLock:
+            IAPManager.instance.BuySet2();
+            break;
+            
+        }
+    }
+}
---
+++ Assets/Script/Scenes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class Scenes : MonoBehaviour
+{
+    public CanvasGroup UICanvas;
+    public GameObject PurchaseCanvas;
+
+    private void Start()
+    {
+        PurchaseCanvas.SetActive(false);
+    }
+
+    public void StartingScene()
+    {
+        SceneManager.LoadScene("StartingScene");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("AlbumTestr");
+    }
+
+    public void ImageSet1()
+    {
+        SceneManager.LoadScene("ImageSet1");
+    }
+
+    public void PurchaseSet()
+    {
+        UICanvas.alpha = .3f;
+        UICanvas.interactable = false;
+        PurchaseCanvas.SetActive(true);
+    }
+
+    public void No()
+    {
+        UICanvas.alpha = 1f;
+        UICanvas.interactable = true;
+        PurchaseCanvas.SetActive(false);
+    }
+
+   public void Exit()
+    {
+        Application.Quit();
+
+    }
+}
