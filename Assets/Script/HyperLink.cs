0461261fadf0a350090afa713da48359cf5e3341
Apostol Cristi <apostol.cristi@yahoo.com>
2020-12-18 19:41:07 +0200

    Finished the project

---
+++ Assets/Script/HyperLink.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HyperLink : MonoBehaviour
+{
+    public void URL()
+    {
+        Application.OpenURL("https://funbible.net/home");
+    }
+}
